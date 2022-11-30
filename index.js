86e90929cc60ad13b20975a99aa499a14e6dc792
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-29 23:17:16 -0500

    Different responses based on different URLs

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const server = http.createServer( (request, response) => {
-    console.log(request.url);
-    response.end('Hello Node.js')
+    if (request.url === '/about') response.end('The about page');
+    else if (request.url === '/contact') response.end('The contact page');
+    else if (request.url === '/') response.end('The home page');
+    else {
+        response.writeHead(404);
+        response.end('page not found')
+    } 
 })
 
 server.listen(3000);
