b3e46117a6b767cc7e54dcc1bf19c73576b7c065
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-11-29 23:06:48 -0500

    Creating our first server

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.js')
+})
+
+server.listen(3000);
