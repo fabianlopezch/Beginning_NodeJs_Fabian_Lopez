081c231d8957c333febb51902b2122f79f3aadb0
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-02 14:47:52 -0500

    Registering a public folder for our static files

--- cleanBlogBased_App/index.js
+++ cleanBlogBased_App/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = new express();
 const PORT = 4000;
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
 });
