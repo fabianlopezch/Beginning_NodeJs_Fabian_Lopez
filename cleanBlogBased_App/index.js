b3a0c31552f5e1c36a1143a932874b0c5903bd91
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-02 15:13:43 -0500

    Setting up the routes for the varius pages

--- cleanBlogBased_App/index.js
+++ cleanBlogBased_App/index.js
@@ -3,9 +3,26 @@
 const express = require('express'); 
 const app = new express();
 const PORT = 4000;
+const path = require('path');
 
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
 });
