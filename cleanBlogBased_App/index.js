7ad9cdc0a8c84afc7180f3c09ef82fec9cb40fe3
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-02 11:10:22 -0500

    Creating a simple Express server in index.js

--- cleanBlogBased_App/index.js
+++ cleanBlogBased_App/index.js
@@ -1,1 +1,9 @@
 // This file is the root of the project
+
+const express = require('express'); 
+const app = new express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
