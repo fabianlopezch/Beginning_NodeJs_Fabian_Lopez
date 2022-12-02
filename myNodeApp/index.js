0a4403010d5e20a449390fa899e18f10da1bbacd
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-01 20:22:19 -0500

    Handling a request with Express

--- myNodeApp/index.js
+++ myNodeApp/index.js
@@ -3,3 +3,7 @@ const app = express();  // calls express function to start new Express app
 app.listen(3000, () => {
     console.log("App listening on port 3000")
 });
+
+app.get('/about', (request, response) => {
+    response.end('About page');
+});
