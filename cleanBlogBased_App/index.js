acadc47c6b1654eaad5638f50c7dd8ecb5d3a71a
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-03 19:31:42 -0500

    Responding to get requests with EJS

--- cleanBlogBased_App/index.js
+++ cleanBlogBased_App/index.js
@@ -3,8 +3,9 @@
 const express = require('express'); 
 const app = new express();
 const PORT = 4000;
-const path = require('path');
+const ejs = require('ejs');
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
@@ -12,17 +13,17 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 });
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 });
