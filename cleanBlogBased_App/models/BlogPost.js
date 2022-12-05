6b2d8b1f1a2256d85721a7cc9e9401e09e4a6d61
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-05 16:30:38 -0500

    Fixing the BlogPost model

--- cleanBlogBased_App/models/BlogPost.js
+++ cleanBlogBased_App/models/BlogPost.js
@@ -1,7 +1,7 @@
 // Model for the blog posts
 
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema();
+const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
     title: String,
