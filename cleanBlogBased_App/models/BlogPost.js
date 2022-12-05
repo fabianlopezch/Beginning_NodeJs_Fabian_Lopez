d878d571e33689a773451e7ef4d0e954b86b278e
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-05 16:21:54 -0500

    Defining a Model in MongoDB

---
+++ cleanBlogBased_App/models/BlogPost.js
@@ -0,0 +1,12 @@
+// Model for the blog posts
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const BlogPostSchema = new Schema({
+    title: String,
+    body: String
+});
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+module.exports = BlogPost;
