2270728eee0327c5f886c6bba9f9180d42443246
Fabian Lopez-Chamorro <fabianlopezch@outlook.com>
2022-12-05 23:40:44 -0500

    Reading data from MongoDB using Mongoose

--- cleanBlogBased_App/test.js
+++ cleanBlogBased_App/test.js
@@ -5,9 +5,43 @@ const BlogPost = require('./models/BlogPost')
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 
+// Creating a new document in BlogPosts collection
 BlogPost.create({
-    title: "The Mythbuster's Guide to SAving Money on energy Bills",
+    title: "The Mythbuster's Guide to Saving Money on energy Bills (last 3)",
     body: "This is the description for this document..."
 }, (error, blogpost) => {
     console.log(error, blogpost);
-})
+});
+
+// Selecting all documents in BlogPosts collection
+BlogPost.find(
+    {}, (error, blogpost) => {
+        console.log(error, blogpost);
+    }
+);
+
+// Finding all documents in BlogPosts collection with a particular title
+BlogPost.find(
+    {title: "The Mythbuster's Guide to Saving Money on energy Bills (last 3)"
+},  (error, blogpost) => {
+        console.log(error, blogpost);
+    }
+);
+
+// Finding all documents in BlogPosts with 'last' in the title
+BlogPost.find(
+    {
+        title: /last 3/
+    },
+    (error, blogpost) => {
+        console.log(error, blogpost);
+    }
+);
+
+// Retrieve single documents with unique id _id
+
+let id = "638ec4f80b716d1a77b127bf";
+
+BlogPost.findById(id, (error, blogpost) => {
+    console.log(error, blogpost);
+});
